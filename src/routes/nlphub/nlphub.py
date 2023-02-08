1043e20e5971f2717bc02a005b1f77ee5278eb26
Wesley Ong <72511481+Wesleyongs@users.noreply.github.com>
2023-02-08 17:15:37 +0800

    Disable nlphub route

--- src/routes/nlphub/nlphub.py
+++ src/routes/nlphub/nlphub.py
@@ -1,5 +1,5 @@
 # Import
-from fastapi import APIRouter, Depends, APIRouter, Query
+from fastapi import APIRouter, Depends, APIRouter, Query, Body
 from sqlalchemy.orm import Session
 from typing import Any, List
 import pandas as pd
@@ -18,12 +18,12 @@ router = APIRouter(
 
 
 @router.post("/", response_model=Any)
-def read_items(
-    inputs: List[str]
-) -> Any:
+def read_items(inputs: List[str] = Body(example=[
+    "Happy Chinese new year everyone! huat ahh!!",
+    "I really hate a certain select minority group",
+    "The current party is doing such a terrible job, bring back donald trump!"
+])) -> Any:
     """
     Retrieve items.
     """
     return crud.get_sentiments(inputs)
-
-
